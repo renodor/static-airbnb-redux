c9b7a28ab8fb7f7e2b888f3c05944aa1dcc51d3e
renaud dor <renaud.dor@gmail.com>
2020-08-13 17:22:38 +0200

    adding google map

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class Map extends Component {
   render() {
-    const style = {
-      height: '100vh'
-    };
+    let marker = null;
+    let center = { lat: 48.856614, lng: 2.352222 };
 
     return (
-      <div className="col-sm-5" style={style}>
-        Map To do
+      <div className="col-sm-5" style={{ height: '100vh' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyA9eczdhAXBjQXg_Y5E8niGj9bVsr237OM' }}
+          center={center}
+          defaultZoom={15}
+        >
+          {marker}
+        </GoogleMapReact>
       </div>
     );
   }
